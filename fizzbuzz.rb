03b804ca0dcc896e9dc0cc6e34fc6ba4615bb7c4
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-21 21:09:52 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
-def fizzbuzz(int)
-  if int % 3 == 0
+def fizzbuzz(15)
+  if 15 % 3 == 0
     "Fizz"
   end
 end
 
-def fizzbuzz(int)
-  if int % 5 == 0 
+def fizzbuzz(15)
+  if 15 % 5 == 0 
     "Buzz"
   end
 end
