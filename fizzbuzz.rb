2c8c3034d90e94724a6979c864e50b5e698eac9a
Nataliya Rudenko <n.rudenko@gmail.com>
2019-01-21 21:17:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(int)
   if 15 / 3 == 5
-    "Fizz"
-  end
-  if 15 / 5 == 3 
-    "Buzz"
+    return "Fizz"
+elsif 15 / 5 == 3 
+   return "Buzz"
+ else 15/(3,5) 
+   return "FizzBuzz"
   end
 end
